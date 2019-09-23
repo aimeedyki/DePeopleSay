aabd41f0029778f587aae6d5fd625bf728b8035c
aimeedyki <akuwudike.a@gmail.com>
2019-09-23 15:13:57 +0100

    update sign in and signup forms

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import CommonModal from '../../common/CommonModal/CommonModal';
-import './Signup.css';
 import { signupValidator } from '../../utils/signupValidation';
 
 import { signupUser } from '../../actions/authAction';
