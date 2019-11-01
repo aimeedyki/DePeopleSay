f29a2931120d267b39b6b66f9c57185bb232272f
aimeedyki <akuwudike.a@gmail.com>
2019-11-02 00:46:03 +0100

    check port issues

--- server/app.js
+++ server/app.js
@@ -45,6 +45,16 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // Require routes into the application.
 app.use('/api/v1', serverRoutes);
 
